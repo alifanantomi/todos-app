91eaa3434dc9e9924f5d72b99d85e680d98230af
alifanantomi <alifanantomi2@gmail.com>
2024-05-27 11:53:49 +0700

    style: tailwind, style todo luist

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <h1>To-Do List</h1>
-    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new task" />
+  <div class="w-1/2 mx-auto flex flex-col py-6 gap-3">
+    <h1 class="text-3xl font-semibold">To-Do List</h1>
+    <div class="flex gap-2">
+      <input class="w-full border-2 border-blue-400 p-2" v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new task" />
+      <button class="bg-blue-400 text-white p-2" @click="addTodo">Tambah</button>
+    </div>
     <ul>
-      <li v-for="(todo, index) in todos" :key="index">
+      <li class="flex gap-2 items-center py-2 border-b-[1px] border-gray-300" v-for="(todo, index) in todos" :key="index">
         <input type="checkbox" v-model="todo.done" />
-        <span :class="{ done: todo.done }">{{ todo.text }}</span>
+        <span :class="{ 'line-through': todo.done }">{{ todo.text }}</span>
         <button @click="removeTodo(index)">Remove</button>
       </li>
     </ul>
